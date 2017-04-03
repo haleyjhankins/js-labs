c4196480694e81c2a99b66c1b4073ad6c18c25e8
haleyjhankins <haley.j.hankins@gmail.com>
2017-04-03 17:45:54 -0500

    finished HL7 section

--- section-1/string-challenge-2-hl7/hl7.js
+++ section-1/string-challenge-2-hl7/hl7.js
@@ -2,26 +2,31 @@ var PIDParser = {
 
   name: function(data) {
      var pieces = data.split('|');
-     console.log(pieces);
-    // return pieces [5];
+     return pieces[5];
+
   },
 
   birthYear: function(data) {
-    // var pieces = data.split('|');
-    // var fullDate = pieces [7];
-    // return fullDate.subtr(0,4);
+    var pieces = data.split('|');
+    console.log(pieces);
+    var date= pieces[7];
+    console.log(date);
+
+    return date.slice(0,4);
   },
 
   birthMonth: function(data) {
-    // var pieces = data.split('|');
-    // var fullDate = pieces [7];
-    // return fullDate.subtr(4,2);
+    var pieces = data.split('|');
+    var date= pieces[7];
+
+    return date.slice(4,6);
   },
 
   birthDay: function(data) {
-    // var pieces =data.split ('|');
-    // var fullDate= pieces [7];
-    // return fullDate.slice (6,8);
+    var pieces = data.split('|');
+    var date= pieces[7];
+
+    return date.slice(6);
   }
 }
 
