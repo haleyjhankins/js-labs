520ef044b195bcf299e2ea1469060d57dd5777eb
haleyjhankins <haley.j.hankins@gmail.com>
2017-04-04 16:43:14 -0500

    finished blackjack challenge

--- section-1/blackjack/blackjack.js
+++ section-1/blackjack/blackjack.js
@@ -1,5 +1,23 @@
-
-
 function countCards(cards) {
+  var cardCount=0;
 
+  for (var i=0; i< cards.length; i++) {
+    if (cards[i].number === 'J' || cards[i].number === 'K' ||      cards[i].number  === 'Q'){
+      cardCount += 10;
+    }
+    else if (cards[i].number !== 'A'){
+      cardCount+= cards[i].number;
+    }
+  }
+
+  for (var j=0; j <cards.length; j++){
+    if(cards[j].number==='A' && cardCount <=10){
+      cardCount+=11;
+    }
+    else if(cards[j].number ==='A' && cardCount >10){
+      cardCount+=1;
+    }
+  }
+
+  return cardCount;
 }
